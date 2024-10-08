0def6526ed2336d1d66c700ee80bd7453b233965
ElCald <eliot.cal@gmail.com>
2024-10-08 10:00:07 +0200

    new version 3

--- langford_v3.cpp
+++ langford_v3.cpp
@@ -21,7 +21,6 @@ int main(int argc, char* argv[]){
     int n = atoi(argv[1]);
     int k = 1; // nb de solutions possible avec cas triviaux
     int val = 0; // valeur à placer dans la case
-    int n_up = n+1; //valeur n mais augmenter pour générer les solutions
     int taille_tab_verif = n*2;
 
     for(int i=0; i<n; i++){ // calcul du nb de solutions totale
@@ -41,75 +40,49 @@ int main(int argc, char* argv[]){
     }
 
     for(int i=0; i<k; i++){
-        tab_solutions[i] = (int*)malloc(sizeof(int)*n_up);
+        tab_solutions[i] = (int*)malloc(sizeof(int)*n);
+        for(int j=0; j<n; j++){ // save de la solution dans le tableau de solutions
+            tab_solutions[i][j] = 0;
+        }
     }
 
     int* tab_verif = (int*)malloc(sizeof(int)*taille_tab_verif); // tableau qui permet de verifier dans la boucle si c'est correct
 
-    // Plages de valeurs pour chaque colonne
-    int* ranges = (int*)malloc(sizeof(int)*n_up);
-    for (int i = 0; i < n_up; i++) {
-        ranges[i] = n_up - 2 + i;  // La plage de chaque colonne augmente
+    int* tab_range = (int*)malloc(sizeof(int)*n); // tableau de solutions individuelle
+
+    /* 
+        Génération du tableau
+    */
+
+    for(int j=0; j<n; j++){
+        tab_range[j] = 0;
     }
 
-    // Génération des combinaisons
-    for (int i = 0; i < k; i++) {
-        val = i;
-        for (int j = 0; j < n_up; j++) {
-            tab_solutions[i][j] = val % ranges[j];  // Affecter la valeur de la colonne
-            val /= ranges[j];  // Mise à jour de l'index temporaire
+    for(int i=0; i<k; i++){
+        
+        for(int j=0; j<n; j++){ // save de la solution dans le tableau de solutions
+            tab_solutions[i][j] = tab_range[j];
+        }
+
+        tab_range[n-1]++;
+        
+        for(int j=n-2; j>=0; j--){
+            if(tab_range[j]%((n-1)+(j-1)) == 0){
+                tab_range[j] = 0;
+                tab_range[j-1]++;
+            }
         }
     }
 
 
     // Affichage du tableau
-    // for (int i = 0; i < k; i++) {
-    //     for (int j = 0; j < n-1; j++) {
-    //         cout << tab_solutions[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-
-    /*
-        Toutes les combinaisons sont générés, il faut maintenant garder tout tester et ne sauvegarder que celle qui sont correctes
-    */
-
-   for (int i = 0; i < k; i++) {        
-
-        int n_insert;
-        bool fail = false;
-
-        for(int a=0; a<taille_tab_verif; a++){
-            tab_verif[a] = 0;
-        }
-
-
+    for (int i = 0; i < k; i++) {
         for (int j = 0; j < n; j++) {
-            n_insert = tab_solutions[i][j]; // indice à placer
-
-            if(tab_verif[n_insert] == 0 && tab_verif[n_insert+n+1] == 0){ // Vérification dans le tableau
-
-                tab_verif[n_insert] = j;
-                tab_verif[n+n_insert+1] = j;
-            }
-            else{
-                fail = true;
-                break;
-            }
+            cout << tab_solutions[i][j] << " ";
         }
-
-
-        if(fail == false){
-            // afficher la ligne i du tableau
-            for(int ii=0; ii<n; ii++){
-                cout << tab_solutions[i][ii] << " ";
-            }
-            cout << endl;
-        }
-
-        
+        cout << endl;
     }
+
 
 
     // Libération de la mémoire
@@ -117,8 +90,7 @@ int main(int argc, char* argv[]){
         free(tab_solutions[i]);
     }
     free(tab_solutions);
-    free(ranges);
-    free(tab_verif);
+
 
     return EXIT_SUCCESS;
 }//fin main
